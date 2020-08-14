84252cdda240517e66812b8a7490b6b06b400e51
nedprz <16900857+nedprz@users.noreply.github.com>
2020-08-14 10:44:08 -0700

    ring buffer passed test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        
+        self.storage = [None]*capacity
+        self.current = 0 
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        if self.current + 1 < self.capacity:
+            self.current += 1
+        else:
+            self.current = 0
 
     def get(self):
-        pass
+        my_list = []
+        for i in self.storage:
+            if i :
+                my_list.append(i)
+        return my_list
